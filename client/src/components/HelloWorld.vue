13514276932b5e8aa5eac5b279a6f40e68d6e7a4
hotratx <mgviero@gmail.com>
2021-09-16 10:34:09 -0300

    server, client view ok menos worker

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,40 +1,97 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+   <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+
+  <b-row>
+  <b-col>
+  </b-col>
+  <b-col>
+  <b-form @submit.stop.prevent="onSubmit" v-if="show">
+    <b-form-group
+      id="input-group-1"
+      label="Number Fib:"
+      label-for="input-1"
+      description="Enter with number to calculate fib"
+    >
+      <b-form-input
+        id="input-1"
+        v-model="form.numero"
+        type="number"
+        placeholder="Enter number"
+        required
+      ></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button @click="getCurrent" variant="danger">Get number</b-button>
+    <b-button @click="getAll" variant="danger">Get all</b-button>
+  </b-form>
+  </b-col>
+  <b-col>
+  </b-col>
+  </b-row>
+
+  {{ this.numbers_cache }}
+
+  {{ this.current }}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      form: {
+        numero: '',
+      },
+      current: '',
+      numbers_cache: [],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.numbers_cache.push(this.form.numero)
+      axios.post('/api/values', {"index": this.form.numero})
+        .then(response => { console.log("sucesso", response) })
+        .catch(error => console.log("deu erro na /api/values/current", error))
+    },
+    getCurrent() {
+      axios.get('/api/values/current')
+        .then(response => { 
+          console.log("get current", response) 
+        })
+        .catch(error => console.log("deu erro na /api/values/current", error))
+    },
+    getAll() {
+      axios.get('/api/values/all')
+        .then(response => { 
+          console.log("get all", response) 
+        })
+        .catch(error => console.log("deu erro na /api/values/all", error))
+    },
+
+    onReset(event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.numero = ''
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
   }
 }
 </script>
